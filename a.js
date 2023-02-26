1fb797d51d8d99a153fd600958de1c365ccfe1d3
rakeshdkd <rakeshd1012@gmail.com>
2023-02-26 12:50:47 +0530

    Delete data from Local Storage

--- a.js
+++ a.js
@@ -29,18 +29,51 @@ heading.classList.remove('heading');
 
 const parent = document.querySelector('.parent');
 const subheading = document.createElement('h4');
-subheading.innerHTML='JAvascript is beauty.';
+subheading.innerHTML = 'JAvascript is beauty.';
 parent.appendChild(heading);
 parent.appendChild(subheading);
 
 
-heading.addEventListener('fullscreenchange')
+// heading.addEventListener('fullscreenchange')
+var div = document.querySelector('.form-control');
+var btn = document.querySelector('#btn');
+btn.style.borderRadius = "4px"
 
+btn.style.backgroundColor = "green"
+btn.style.color = "white"
 
+var Name = document.querySelector('#name')
+var email = document.querySelector('#email');
 
+btn.addEventListener('click', (Event) => {
+    // console.log(Name.value);
+    // console.log(email.value);
+    var li = document.createElement('li');
+    li.className = "Newitem";
+    li.style.marginTop = "20px"
+    li.innerHTML = Name.value + " " + email.value;
+    div.insertAdjacentElement('beforeend', li);
+    var del = document.createElement('button');
+    del.className = "delet-button";
+    del.style.marginLeft = "5px"
+    del.style.borderRadius = "4px"
+    del.style.backgroundColor = "red"
+    del.innerHTML = "Delet"
+    li.insertAdjacentElement('beforeend', del)
 
+    var delet = document.querySelector('.delet-button');
 
+    localStorage.setItem('Name', Name.value)
+    localStorage.setItem('Email', email.value);
 
+    delet.addEventListener('click', (Event) => {
+        li.remove();
+        del.remove();
+
+        localStorage.removeItem('Name')
+        localStorage.removeItem('Email');
+    });
+});
 
 
 
